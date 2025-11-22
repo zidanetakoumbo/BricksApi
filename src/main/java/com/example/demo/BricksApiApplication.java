e28090dc283fb700b2aade41d38071db57da44ba
zidanetakoumbo <zidtakoumbo@gmail.com>
2025-11-22 03:34:18 +0100

    Premier commit bricksApi

---
+++ src/main/java/com/example/demo/BricksApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BricksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BricksApiApplication.class, args);
+	}
+
+}
