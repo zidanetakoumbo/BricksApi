bbc2ff9acdb80e28ce8a2cd1c52fa5b38c3d113b
zidanetakoumbo <zidtakoumbo@gmail.com>
2025-11-25 05:10:23 +0100

    initial commit

---
+++ src/main/java/application/BricksAPI/BricksApiApplication.java
@@ -0,0 +1,12 @@
+package application.BricksAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BricksApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BricksApiApplication.class, args);
+	}
+}
---
+++ src/test/java/application/BricksAPI/BricksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package application.BricksAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BricksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
